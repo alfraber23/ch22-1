9433b353a52fa9fde51ff0b2d50a7f35ea7b14d3
Beto <josealberto.fb15@gmail.com>
2023-01-31 13:16:59 -0600

    feat: java03-variables

---
+++ java/HolaCrayoli/src/saludo/HolaCrayoli.java
@@ -0,0 +1,16 @@
+package saludo;
+
+public class HolaCrayoli {
+
+	public static void main(String[] args) {
+		// Esto es un comentario en linea
+		
+		/**
+		 * Este es un comtario
+		 * de bloque
+		 */
+		System.out.println("Hola Crayoli"); // se pone ln para un salto de linea
+		System.out.println("Estoy en la CH22");
+	}
+
+}
---
+++ java/Java03-variables/src/mx/generation/Variables.java
@@ -0,0 +1,24 @@
+package mx.generation;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		/**
+		 * En java existen 8 datos primitivos:
+		 * -byte: tipo numerico de 8 bits
+		 * -short: tipo numerico de 16 bits
+		 * -int: tipo numerico de 32 bits
+		 * -long: tipo numerico de 64 bits
+		 * -float: tipo numerico de punto flotante de 32 bits
+		 * -double: tipo numerico de punto flotante de 64 bits
+		 * -boolean: dato boleano (true o false)
+		 * -char: caracter
+		 */
+		
+		//definimos una variable de tipo byte
+		byte variableTipoByte = 10;
+		System.out.println("Byte: " + variableTipoByte);
+	}
+	
+	
+}
