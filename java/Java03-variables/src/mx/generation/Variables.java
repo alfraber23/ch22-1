6ab66fc359709971dd94420eb420d9b668636948
Beto <josealberto.fb15@gmail.com>
2023-01-31 13:40:07 -0600

    ci: tipos de datos impresos en consola

--- java/Java03-variables/src/mx/generation/Variables.java
+++ java/Java03-variables/src/mx/generation/Variables.java
@@ -15,9 +15,29 @@ public class Variables {
 		 * -char: caracter
 		 */
 		
-		//definimos una variable de tipo byte
+		//definimos una variable de tipo byte (8 bits)
 		byte variableTipoByte = 10;
 		System.out.println("Byte: " + variableTipoByte);
+		System.out.println("Max byte value: " + Byte.MAX_VALUE);
+		System.out.println("Min byte value: " + Byte.MIN_VALUE);
+		
+		//Definimos una variable de tipo short (16 bits)
+		short variableTipoShort = 125;
+		System.out.println("Short: " + variableTipoShort);
+		System.out.println("Max short value: " + Short.MAX_VALUE);
+		System.out.println("Min short value: " + Short.MIN_VALUE);
+		
+		//Definimos una variable de tipo int (32 bits)
+		int variableTipoInt = 1256;
+		System.out.println("Int: " + variableTipoInt);
+		System.out.println("Max int value: " + Integer.MAX_VALUE);
+		System.out.println("Min int value: " + Integer.MIN_VALUE);
+		
+		//Declaramos una variable de tipo long (64 bits)
+		long variableTipoLong = 123456;
+		System.out.println("Long: " + variableTipoLong);
+		System.out.println("Max long value: " + Long.MAX_VALUE);
+		System.out.println("Min long value: " + Long.MIN_VALUE);
 	}
 	
 	
